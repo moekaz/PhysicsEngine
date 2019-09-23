bd7dd2bc86b0ba52a694f545aa2997dc34aac345
Mohamed Kazma <mohamed.kazma@carleton.ca>
2019-09-22 21:49:38 -0400

    added square macro

--- MTRXEngine/MTRXEngine/source/headers/Defs.h
+++ MTRXEngine/MTRXEngine/source/headers/Defs.h
@@ -7,6 +7,8 @@
 
 namespace mtrx
 {	
+	#define SQR(x) x * x // Easy way of squaring
+	
 	#define LOGGER_FILE_SIZE 5242880 // Maximum size of logger file 
 	#define PI 3.14159265358f // Approximation of pi
 	#define WORLD_DIMENSIONS 3 // World dimensions
